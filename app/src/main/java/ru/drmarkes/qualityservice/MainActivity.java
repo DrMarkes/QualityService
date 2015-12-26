77bcb935febb5cd911e691ea0359e3a0d74149f5
DrMarkes <markovandrew@yandex.ru>
2015-12-27 00:40:09 +0700

    Создан класс для хранения введеных значений, написана обработка нажатий кнопок

--- app/src/main/java/ru/drmarkes/qualityservice/MainActivity.java
+++ app/src/main/java/ru/drmarkes/qualityservice/MainActivity.java
@@ -2,11 +2,13 @@ package ru.drmarkes.qualityservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.widget.Button;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Smile smile = new Smile();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +17,51 @@ public class MainActivity extends AppCompatActivity {
         initButtons();
     }
 
+    /**
+     * Method creates a button and registers the listener clicks
+     */
     private void initButtons() {
-        ImageButton button = (ImageButton)findViewById(R.id.positive);
+        ImageButton buttonPositive = (ImageButton)findViewById(R.id.positive);
+        buttonPositive.setOnClickListener(this);
+
+        ImageButton buttonNeutral = (ImageButton)findViewById(R.id.neutral);
+        buttonNeutral.setOnClickListener(this);
+
+        ImageButton buttonNegative = (ImageButton)findViewById(R.id.negative);
+        buttonNegative.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast toast;
+        switch (v.getId()) {
+            case R.id.positive:
+                toast = Toast.makeText(getApplicationContext(), "Спасибо за ваш положительный отзыв, " +
+                                "мы всегда рады Вас видеть!",
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                smile.increaseCountPositive();
+                break;
+            case R.id.neutral:
+                toast = Toast.makeText(getApplicationContext(), "Спасибо за ваш нейтральный отзыв, " +
+                                "мы постоянно улучшаем качество обслуживания!",
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                smile.increaseCountNeutral();
+                break;
+            case R.id.negative:
+                toast = Toast.makeText(getApplicationContext(), "Спасибо за ваш отрицательный отзыв, " +
+                                "мы учтем Ваши пожелания!",
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                smile.increaseCountNegative();
+                break;
+        }
+        toast = Toast.makeText(getApplicationContext(), smile.getQuality(), Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP, 0, 0);
+        toast.show();
     }
 }
---
+++ app/src/main/java/ru/drmarkes/qualityservice/Smile.java
@@ -0,0 +1,46 @@
+package ru.drmarkes.qualityservice;
+
+/**
+ * Created by Андрей on 26.12.2015.
+ */
+public class Smile {
+    private int countPositive;
+    private int countNeutral;
+    private int countNegative;
+
+    public Smile() {
+        countNeutral = 0;
+        countNegative = 0;
+        countPositive = 0;
+    }
+
+    public void increaseCountPositive() {
+        countPositive++;
+    }
+
+    public void increaseCountNeutral() {
+        countNeutral++;
+    }
+
+    public void increaseCountNegative() {
+        countNegative++;
+    }
+
+    public int getCountPositive() {
+        return countPositive;
+    }
+
+    public int getCountNeutral() {
+        return countNeutral;
+    }
+
+    public int getCountNegative() {
+        return countNegative;
+    }
+
+    public String getQuality() {
+        return "Позитивно: " + getCountPositive() +
+                "; Нейтрально: " + getCountNeutral() +
+                "; Негативно: " + getCountNegative();
+    }
+}

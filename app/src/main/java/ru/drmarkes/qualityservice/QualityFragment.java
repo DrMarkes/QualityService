47807703d8ef55c957b01c2ec086ece7c9fe9862
DrMarkes <markovandrew@yandex.ru>
2016-01-06 11:32:57 +0700

    Отрисовка диаграммы начало

--- app/src/main/java/ru/drmarkes/qualityservice/QualityFragment.java
+++ app/src/main/java/ru/drmarkes/qualityservice/QualityFragment.java
@@ -1,5 +1,6 @@
 package ru.drmarkes.qualityservice;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -8,6 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
@@ -18,6 +27,7 @@ public class QualityFragment extends Fragment implements MainActivity.onChahgedS
     private Smile smile;
     private ArrayList<Float> values = new ArrayList<>(3);
     private View qualityView;
+    PieChart chart;
 
     public static QualityFragment newInstance(Smile smile) {
         Bundle args = new Bundle();
@@ -29,53 +39,78 @@ public class QualityFragment extends Fragment implements MainActivity.onChahgedS
 
     public ArrayList<Float> getValues() {
         values.add(0, (float) smile.getCountPositive());
-        values.add(1, (float) smile.getCountNegative());
-        values.add(2, (float) smile.getCountNeutral());
+        values.add(1, (float) smile.getCountNeutral());
+        values.add(2, (float) smile.getCountNegative());
         return  values;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         qualityView = inflater.inflate(R.layout.page, container, false);
+        chart = (PieChart) qualityView.findViewById(R.id.chart);
         drawQuality();
         return qualityView;
     }
 
     public void drawQuality() {
         getValues();
-        // values = calculateData(values);
-        TextView textPositive = (TextView)qualityView.findViewById(R.id.textViewPositive);
-        textPositive.setText("Позитивных отзывов: " + Float.toString(values.get(0)));
 
-        TextView textNeutral = (TextView)qualityView.findViewById(R.id.textViewNeutral);
-        textNeutral.setText("Нейтральных отзывов: " + Float.toString(values.get(2)));
+        chart.setDescription("");
+        chart.setDragDecelerationFrictionCoef(0.95f);
+        setData(3);
+    }
 
-        TextView textNegative = (TextView)qualityView.findViewById(R.id.textViewNegative);
-        textNegative.setText("Негативных отзывов: " + Float.toString(values.get(1)));
+    private void setData(int count) {
+
+        String[] mParties = new String[] {
+                "Положительные отзыв", "Нейтральные отзывы", "Отрицательные отзывы"
+        };
+
+        ArrayList<Entry> yVals1 = new ArrayList<>();
+
+        // IMPORTANT: In a PieChart, no values (Entry) should have the same
+        // xIndex (even if from different DataSets), since no values can be
+        // drawn above each other.
+        for (int i = 0; i < count; i++) {
+            yVals1.add(new Entry(values.get(i), i));
+        }
+
+        ArrayList<String> xVals = new ArrayList<>();
+
+        for (int i = 0; i < count; i++)
+            xVals.add("");
+
+        PieDataSet dataSet = new PieDataSet(yVals1, "Результаты опроса");
+    //    dataSet.setSliceSpace(2f);
+    //    dataSet.setSelectionShift(5f);
+
+        // add a lot of colors
+
+        ArrayList<Integer> colors = new ArrayList<>();
+
+        colors.add(Color.YELLOW);
+
+        colors.add(Color.GREEN);
+
+        colors.add(Color.RED);
+
+        dataSet.setColors(colors);
+    //    dataSet.setSelectionShift(0f);
+
+        PieData data = new PieData(xVals, dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.WHITE);
+        chart.setData(data);
+
+     //   chart.highlightValues(null);
+
+        chart.invalidate();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    private ArrayList<Float> calculateData(ArrayList<Float> data) {
-        // TODO Auto-generated method stub
-        float total = 0;
-        for (float value: data) {
-            total += value;
-        }
-
-        if(total == 0) {
-            total = 1;
-        }
-
-        for (int i = 0; i < data.size(); i++) {
-            float calculateData = 360 * (data.get(i) / total);
-            data.set(i, calculateData);
-
-        }
-        return data;
     }
 
     @Override
